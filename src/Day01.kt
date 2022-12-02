a91d2bd30d6161b847a317dd239b0145ae5228a1
Alex Munda <amunda@hashicorp.com>
2022-12-01 20:20:43 -0600

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,27 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(calorieSumsByElf: List<Int>): Int {
+        return calorieSumsByElf.max()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(calorieSumsByElf: List<Int>): Int {
+        return calorieSumsByElf.sortedDescending().take(3).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val input = readInput("Day01").iterator()
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val calories = mutableListOf<Int>()
+    var currentCalCount = 0
+
+    while (input.hasNext()) {
+        val i = input.next()
+        if (i.isEmpty()) {
+            calories.add(currentCalCount)
+            currentCalCount = 0
+        } else {
+            currentCalCount += i.toInt()
+        }
+    }
+
+    println(part1(calories))
+    println(part2(calories))
 }
